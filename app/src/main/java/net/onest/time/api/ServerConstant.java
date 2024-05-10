90f5eaeb547160b935c95581bc69a73edacce923
LingFenglong <2808021998@qq.com>
2024-05-10 15:27:03 +0800

    Api调用

---
+++ app/src/main/java/net/onest/time/api/ServerConstant.java
@@ -0,0 +1,7 @@
+package net.onest.time.api;
+
+public class ServerConstant {
+    public static final String HOST = "http://8.130.17.7";
+    public static final String PORT = "80";
+    public static final String ADDRESS = HOST + ":" + PORT;
+}
