efeacb6f4f4a910b3be9357b61afdd82f396ebc3
LingFenglong <2808021998@qq.com>
2024-05-27 17:29:07 +0800

    待办页面日历右上角的小点

--- app/src/main/java/net/onest/time/utils/DateUtil.kt
+++ app/src/main/java/net/onest/time/utils/DateUtil.kt
@@ -92,59 +92,25 @@ class DateUtil {
             calendar[Calendar.SECOND] = 0
             calendar[Calendar.MILLISECOND] = 0
             return calendar.toInstant().epochSecond * 1000
-            //        LocalDate localDate = LocalDate.of(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
-//        return 1000 * (localDate.toEpochDay() * 60 * 60 * 24 - 8 * 60 * 60);
-        } //    public enum Unit {
-        //        DAY(1), WEEK(7), MONTH(30), YEAR(365);
-        //
-        //        private final Integer days;
-        //
-        //        Unit(Integer days) {
-        //            this.days = days;
-        //        }
-        //
-        //        public long getStartEpochSecond() {
-        //            Calendar calendar = Calendar.getInstance();
-        //            switch (this) {
-        //                case DAY:
-        //                    calendar.set(Calendar.HOUR_OF_DAY, 0);
-        //                    calendar.set(Calendar.MINUTE, 0);
-        //                    calendar.set(Calendar.SECOND, 0);
-        //                    calendar.set(Calendar.MILLISECOND, 0);
-        //                    return calendar.toInstant().getEpochSecond();
-        //                case WEEK:
-        //                    calendar.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY);
-        //                    calendar.set(Calendar.HOUR_OF_DAY, 0);
-        //                    calendar.set(Calendar.MINUTE, 0);
-        //                    calendar.set(Calendar.SECOND, 0);
-        //                    return calendar.getTime().toInstant().getEpochSecond();
-        //                case MONTH:
-        //                    calendar.set(calendar.get(Calendar.YEAR),
-        //                            calendar.get(Calendar.MONTH),
-        //                            calendar.get(Calendar.DAY_OF_MONTH),
-        //                            0, 0, 0);
-        //                    calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMinimum(Calendar.DAY_OF_MONTH));
-        //                    return calendar.getTime().toInstant().getEpochSecond();
-        //                case YEAR:
-        //            }
-        //            return 0;
-        //        }
-        //
-        //        public long getEndEpochSecond() {
-        //            return LocalDate.now(ZoneId.of("UTC+8")).(LocalTime.MIN, ZoneOffset.of("+8"));
-        //        }
-        //
-        //        public Integer getDays() {
-        //            return days;
-        //        }
-        //    }
-        //点击增加时:
-        //获取下一日的日期:
-        //获取下一周的日期:
-        //获取下一月的日期:
-        //点击减少时:
-        //获取上一日的日期:
-        //获取上一周的日期:
-        //获取上一月的日期:
+        }
+
+        /**
+         * 获得指定日期的 EpochMillisecond
+         */
+        @JvmStatic
+        fun epochMillisecond(calendar: Calendar): Long {
+            calendar[Calendar.HOUR_OF_DAY] = 0
+            calendar[Calendar.MINUTE] = 0
+            calendar[Calendar.SECOND] = 0
+            calendar[Calendar.MILLISECOND] = 0
+            return calendar.toInstant().epochSecond * 1000
+        }
+
+        /**
+         * 获得指定日期的 EpochMillisecond
+         */
+        @JvmStatic
+        fun epochMillisecond(millisecond: Long) = epochMillisecond(Date(millisecond))
+
     }
 }
