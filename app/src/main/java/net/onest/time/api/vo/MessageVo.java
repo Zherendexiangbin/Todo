06bfb96fd4160899556819b0e7ca88a3308046d7
yuxin <1241250055@qq.com>
2024-06-03 19:59:41 +0800

    新增 MessageVo 修改聊天返回类型

---
+++ app/src/main/java/net/onest/time/api/vo/MessageVo.java
@@ -0,0 +1,78 @@
+package net.onest.time.api.vo;
+
+import java.util.Date;
+
+public class MessageVo {
+    private Long messageId;
+    private Long fromUserId;
+    private Long toUserId;
+    private Long toRoomId;
+    private String content;
+    private Date sendTime;
+    private String fromUserName;
+    private String fromUserAvatar;
+
+    public Long getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(Long messageId) {
+        this.messageId = messageId;
+    }
+
+    public Long getFromUserId() {
+        return fromUserId;
+    }
+
+    public void setFromUserId(Long fromUserId) {
+        this.fromUserId = fromUserId;
+    }
+
+    public Long getToUserId() {
+        return toUserId;
+    }
+
+    public void setToUserId(Long toUserId) {
+        this.toUserId = toUserId;
+    }
+
+    public Long getToRoomId() {
+        return toRoomId;
+    }
+
+    public void setToRoomId(Long toRoomId) {
+        this.toRoomId = toRoomId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getSendTime() {
+        return sendTime;
+    }
+
+    public void setSendTime(Date sendTime) {
+        this.sendTime = sendTime;
+    }
+
+    public String getFromUserName() {
+        return fromUserName;
+    }
+
+    public void setFromUserName(String fromUserName) {
+        this.fromUserName = fromUserName;
+    }
+
+    public String getFromUserAvatar() {
+        return fromUserAvatar;
+    }
+
+    public void setFromUserAvatar(String fromUserAvatar) {
+        this.fromUserAvatar = fromUserAvatar;
+    }
+}
