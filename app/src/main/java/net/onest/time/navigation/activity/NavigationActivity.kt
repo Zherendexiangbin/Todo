08c06a959b974cb72fe4820d3209469ee4a3d036
LingFenglong <2808021998@qq.com>
2024-06-03 14:34:19 +0800

    通过文本选择添加任务

--- app/src/main/java/net/onest/time/navigation/activity/NavigationActivity.kt
+++ app/src/main/java/net/onest/time/navigation/activity/NavigationActivity.kt
@@ -1,32 +1,36 @@
-package net.onest.time.navigation.activity;
+package net.onest.time.navigation.activity
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import net.onest.time.R
 
-import android.os.Bundle;
-import android.view.View;
+class NavigationActivity : AppCompatActivity() {
+    var bottomNavigation: BottomNavigationView? = null
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        setContentView(R.layout.activity_navigation)
 
-import net.onest.time.R;
-
-public class NavigationActivity extends AppCompatActivity {
-
-    BottomNavigationView bottomNavigation;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-        setContentView(R.layout.activity_navigation);
-
-        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation = findViewById(R.id.bottom_navigation)
 
         //获取navController
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+
         //通过setupWithNavController将底部导航和导航控制器进行绑定
-        NavigationUI.setupWithNavController(bottomNavigation, navController);
+        NavigationUI.setupWithNavController(bottomNavigation!!, navController)
+
+        val taskName = intent?.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)
+        taskName?.run {
+            val bundle = Bundle()
+            bundle.putString("taskName", this.toString())
+
+            navController.navigate(R.id.todo_fragment, bundle)
+        }
     }
 }
