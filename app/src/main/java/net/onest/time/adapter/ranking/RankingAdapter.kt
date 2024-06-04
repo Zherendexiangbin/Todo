9075405984c11b8b15b9432ae8fa4c57de9ed1b8
LingFenglong <2808021998@qq.com>
2024-06-04 09:05:17 +0800

    排行榜前三添加奖牌

--- app/src/main/java/net/onest/time/adapter/ranking/RankingAdapter.kt
+++ app/src/main/java/net/onest/time/adapter/ranking/RankingAdapter.kt
@@ -1,11 +1,14 @@
 package net.onest.time.adapter.ranking
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import net.onest.time.R
 import net.onest.time.api.vo.UserVo
 import net.onest.time.databinding.RankingItemBinding
 
@@ -21,6 +24,8 @@ class RankingAdapter(
         val signature = binding.signature
         val tomatoDuration = binding.tomatoDuration
         val avatar = binding.avatar
+        val card = binding.card
+        val rankingLayout = binding.rankingLayout
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -32,6 +37,15 @@ class RankingAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val userVo = rankingList[position]
+        val imageId: Int? = when (position) {
+            0 -> R.drawable.ranking_first
+            1 -> R.drawable.ranking_second
+            2 -> R.drawable.ranking_third
+            else -> null
+        }
+        imageId?.let {
+            holder.rankingLayout.background = ContextCompat.getDrawable(context, imageId)
+        }
 
         holder.ranking.text = (position + 1).toString()
         holder.username.text = userVo.userName
