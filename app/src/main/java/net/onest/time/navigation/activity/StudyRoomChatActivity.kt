533804aa2a10cf4d9e118aa43b70a5f5c962b737
6iuzD <513224lzd>
2024-06-06 10:27:26 +0800

    部分优化

--- app/src/main/java/net/onest/time/navigation/activity/StudyRoomChatActivity.kt
+++ app/src/main/java/net/onest/time/navigation/activity/StudyRoomChatActivity.kt
@@ -14,6 +14,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
@@ -63,6 +64,7 @@ class StudyRoomChatActivity : AppCompatActivity() {
     private var editMessage: EditText? = null
     private var smartRefreshLayout: SmartRefreshLayout? = null
     private var checkIn: Button? = null
+    private var tools: LinearLayout? = null
 
     private lateinit var binding: ActivityStudyRoomChatBinding
 
@@ -280,6 +282,8 @@ class StudyRoomChatActivity : AppCompatActivity() {
         btnSend = findViewById(R.id.btn_send)
 
         checkIn = findViewById(R.id.check_in)
+        tools = findViewById(R.id.tools)
+        tools!!.visibility = View.GONE
     }
 
     // 下拉刷新获取消息
